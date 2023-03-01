a04454647b8161dcb4d1ce3260e0e61339397f10
israel silva <Israel524.is@gmail.com>
2023-03-01 20:30:06 -0300

    aula 404
    Armazenando consultas em cache parte 1

--- app/Http/Controllers/NoticiaController.php
+++ app/Http/Controllers/NoticiaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Noticia;
 use App\Http\Requests\StoreNoticiaRequest;
 use App\Http\Requests\UpdateNoticiaRequest;
+use Illuminate\Support\Facades\Cache;
 
 class NoticiaController extends Controller
 {
@@ -15,7 +16,16 @@ class NoticiaController extends Controller
      */
     public function index()
     {
-        $noticias = Noticia::orderByDesc('created_at')->limit(10)->get();
+        $noticias = [];
+        //$noticias = Noticia::orderByDesc('created_at')->limit(10)->get();
+
+        //criar um dado dentro do bd Redis
+        Cache::put('site', 'jorgesantana.net.br', 10);
+        //chave, valor, tempo em segundos para expirar o dado em memória
+
+        //recuperar um dado dentro do bd Redis
+        $site = Cache::get('site');
+        echo $site;
 
         return view('noticia', ['noticias' => $noticias]);
     }
